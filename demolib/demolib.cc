8391ed394e56cc1b2b7e1628b72985a17834bedc
Chris Grieger <chris@scolp.de>
2015-01-21 14:56:51 +0100

    Use 'puts' instead of 'printf'

--- demolib/demolib.cc
+++ demolib/demolib.cc
@@ -2,11 +2,11 @@
 
 void print() {
   #ifdef _WIN32
-    printf("Hello World, Windows!");
+    puts("Hello World, Windows!");
   #elif defined __linux__
-    printf("Hello World, Linux!");
+    puts("Hello World, Linux!");
   #elif defined TARGET_OS_MAC
-    printf("Hello World, MacOS!");
+    puts("Hello World, MacOS!");
   #else
   #error "unknown platform"
   #endif
